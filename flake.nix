5e1834af82c4ffead512673aa09382bd5df8d75d
hugosenari <hugosenari@gmail.com>
2022-05-12 11:53:03 -0300

    chore: re index jesterjson

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."jesterjson-master".dir   = "master";
+  inputs."jesterjson-master".owner = "nim-nix-pkgs";
+  inputs."jesterjson-master".ref   = "master";
+  inputs."jesterjson-master".repo  = "jesterjson";
+  inputs."jesterjson-master".type  = "github";
+  inputs."jesterjson-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jesterjson-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jesterjson-v1_0_1".dir   = "v1_0_1";
+  inputs."jesterjson-v1_0_1".owner = "nim-nix-pkgs";
+  inputs."jesterjson-v1_0_1".ref   = "master";
+  inputs."jesterjson-v1_0_1".repo  = "jesterjson";
+  inputs."jesterjson-v1_0_1".type  = "github";
+  inputs."jesterjson-v1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jesterjson-v1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
